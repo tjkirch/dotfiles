f94863086f081c8c68556442f0312209e09a5ca0
tjkirch <tjkirch+github@gmail.com>
2010-10-05 21:57:18 +0000

    Always source ssh-agent settings if available

--- dot.oh-my-zsh/custom/environment.zsh
+++ dot.oh-my-zsh/custom/environment.zsh
@@ -8,4 +8,6 @@ export GOBIN="${HOME}/code/go/lang/bin"
 
 export PATH="${PATH}:${HOME}/local/bin:/usr/sbin:/sbin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/.cljr/bin"
 
+[ -x /usr/bin/ssh-agent ] && eval `ssh-agent -s` >/dev/null
+
 if [[ -s /home/tjkirch/.rvm/scripts/rvm ]] ; then source /home/tjkirch/.rvm/scripts/rvm ; fi

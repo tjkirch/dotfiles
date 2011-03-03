e51e2d5244f8b5d3c2045932987741bda7860b17
tjkirch <tjkirch+github@gmail.com>
2011-03-03 20:54:33 +0000

    Set default browser to chrome

--- dot.oh-my-zsh/custom/environment.zsh
+++ dot.oh-my-zsh/custom/environment.zsh
@@ -6,6 +6,8 @@ export GOOS="linux"
 export GOARCH="amd64"
 export GOBIN="${HOME}/code/go/lang/bin"
 
+export BROWSER="chrome"
+
 export PATH="${PATH}:${HOME}/local/bin:/usr/sbin:/sbin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/local/opt/msp430-gcc-4.4.4/bin/"
 
 if [[ -s /home/tjkirch/.rvm/scripts/rvm ]] ; then source /home/tjkirch/.rvm/scripts/rvm ; fi

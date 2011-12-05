6bca770aae36409e2eaacee06f521e4f4978fdc5
Tom Kirchner <tjkirch+github@gmail.com>
2011-12-05 00:01:28 -0800

    Update PATH to include /usr/local/bin

--- dot.oh-my-zsh/custom/environment.zsh
+++ dot.oh-my-zsh/custom/environment.zsh
@@ -8,7 +8,7 @@ export GOBIN="${HOME}/code/go/lang/bin"
 
 export BROWSER="firefox"
 
-export PATH="${PATH}:${HOME}/local/bin:/usr/sbin:/sbin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/local/opt/msp430-gcc-4.4.4/bin/"
+export PATH="${HOME}/local/bin:/usr/local/bin:/usr/sbin:/sbin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/local/opt/msp430-gcc-4.4.4/bin/:${PATH}"
 export MANPATH="${MANPATH}:${HOME}/local/share/man"
 
 if [[ -s /home/tjkirch/.rvm/scripts/rvm ]] ; then source /home/tjkirch/.rvm/scripts/rvm ; fi

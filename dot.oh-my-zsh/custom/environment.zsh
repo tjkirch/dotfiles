a36c692852a27e208b3b670888903b58097215f2
Tom Kirchner <tjkirch+github@gmail.com>
2012-04-22 20:09:57 -0700

    Add RVM to PATH and fix sourcing

--- dot.oh-my-zsh/custom/environment.zsh
+++ dot.oh-my-zsh/custom/environment.zsh
@@ -8,7 +8,7 @@ export CCACHE_DIR="${HOME}/.ccache/"
 
 export BROWSER="firefox"
 
-export PATH="${HOME}/local/bin:${HOME}/.cabal/bin:${HOME}/.lein/bin:/usr/local/bin:${PATH}:/usr/sbin:/sbin"
+export PATH="${HOME}/local/bin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/.rvm/bin:/usr/local/bin:${PATH}:/usr/sbin:/sbin"
 export MANPATH="${HOME}/local/share/man:${MANPATH}"
 
-if [[ -s /home/tjkirch/.rvm/scripts/rvm ]] ; then source /home/tjkirch/.rvm/scripts/rvm ; fi
+if [[ -s ${HOME}/.rvm/scripts/rvm ]] ; then source ${HOME}/.rvm/scripts/rvm ; fi

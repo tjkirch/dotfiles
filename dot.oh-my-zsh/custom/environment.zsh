5d692edbce98d0a0eb0ac25fd43c893eea304836
Tom Kirchner <tjkirch+github@gmail.com>
2014-10-17 17:26:54 -0700

    Add default /usr/local pkgconfig path

--- dot.oh-my-zsh/custom/environment.zsh
+++ dot.oh-my-zsh/custom/environment.zsh
@@ -18,4 +18,6 @@ export PAGER="less -FRX"
 export PATH="${HOME}/local/bin:${HOME}/.cabal/bin:${HOME}/.lein/bin:${HOME}/.rvm/bin:/usr/local/bin:${PATH}:/usr/sbin:/sbin"
 export MANPATH="${HOME}/local/share/man:${MANPATH}"
 
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig/:/usr/local/share/pkgconfig"
+
 if [[ -s ${HOME}/.rvm/scripts/rvm ]] ; then source ${HOME}/.rvm/scripts/rvm ; fi

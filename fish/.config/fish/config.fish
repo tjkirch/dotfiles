581d5b2166abff4fcd5afa3926b76846a2490d7c
Tom Kirchner <tjk@amazon.com>
2020-07-23 15:12:30 -0700

    Make fish REPORTTIME equivalent more specific

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,17 @@ end
 # (event functions cannot be autoloaded, must be defined here)
 function _report_time -d "Reports runtime of long commands" --on-event fish_postexec
    if test "$CMD_DURATION" -gt 5000
-      echo (set_color -io -b black black)took $CMD_DURATION milliseconds: $argv[1](set_color normal)
+      set milliseconds (math -s0 "($CMD_DURATION) % 1000")
+      set seconds      (math -s0 "($CMD_DURATION / (1000)) % 60")
+      set minutes      (math -s0 "($CMD_DURATION / (1000*60)) % 60")
+      set hours        (math -s0 "($CMD_DURATION / (1000*60*60)) % 24")
+      set days         (math -s0 "($CMD_DURATION / (1000*60*60*24))")
+
+      echo -n (set_color -io -b black black)"Took "
+      test $days -gt 0 && echo -n "$days days, "
+      test $hours -gt 0 && echo -n "$hours hours, "
+      test $minutes -gt 0 && echo -n "$minutes minutes, "
+      printf "%d.%03d seconds: %s" $seconds $milliseconds $argv[1]
+      echo (set_color normal)
    end
 end

fc2a6377d2d1b33e8291c52f0dccfa22524f391e
Tom Kirchner <tjk@amazon.com>
2020-07-31 11:49:53 -0700

    Switch to shared agent script

--- fish/.config/fish/functions/agent.fish
+++ fish/.config/fish/functions/agent.fish
@@ -1,12 +1,4 @@
-# Defined in /tmp/fish.Iq542q/agent.fish @ line 2
+# Defined in /tmp/fish.Wix7jT/agent.fish @ line 2
 function agent --wraps=fixme --description 'Starts SSH agent'
-   pkill -x ssh-agent
-   ssh-agent > ~/.ssh/environment-$hostname
-   sed -e '/echo /d' \
-       -e 's/export .*//' \
-       -e 's/\(.*\)=\(.*\)/set -gx \1 \2/;' \
-       ~/.ssh/environment-$hostname \
-       > ~/.ssh/environment-$hostname.fish
-   eval (cat ~/.ssh/environment-$hostname.fish)
-   ssh-add ~/.ssh/id_rsa ~/.ssh/github-ed25519
+   command agent && eval (cat ~/.ssh/environment-$hostname.fish)
 end

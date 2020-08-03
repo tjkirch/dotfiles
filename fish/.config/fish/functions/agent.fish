4d186282d6c997b9fc20c0eab7c5eace6eb7ba94
Tom Kirchner <git@halffull.org>
2020-08-02 20:15:25 -0700

    eval ssh agent config if exists, not if agent command ran
    The agent command may return nonzero because it lists keys I don't have on
    every machine, but as long as it created a non-empty environment file, try to
    eval it.

--- fish/.config/fish/functions/agent.fish
+++ fish/.config/fish/functions/agent.fish
@@ -1,4 +1,5 @@
-# Defined in /tmp/fish.Wix7jT/agent.fish @ line 2
+# Defined in /tmp/fish.BNdvbf/agent.fish @ line 2
 function agent --wraps=fixme --description 'Starts SSH agent'
-   command agent && eval (cat ~/.ssh/environment-$hostname.fish)
+   command agent
+   test -s ~/.ssh/environment-$hostname.fish && eval (cat ~/.ssh/environment-$hostname.fish)
 end

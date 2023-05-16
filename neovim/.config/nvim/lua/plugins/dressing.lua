b200ba9bf6a4818dca2ad7dec06235c771616088
Tom Kirchner <git@halffull.org>
2023-05-15 17:29:37 -0700

    neovim: disable dressing transparency

--- neovim/.config/nvim/lua/plugins/dressing.lua
+++ neovim/.config/nvim/lua/plugins/dressing.lua
@@ -1,4 +1,9 @@
 return {
    'stevearc/dressing.nvim',
    lazy=true,
+   opts = {
+      -- I dislike its transparency
+      input = { win_options = { winblend = 0 } },
+      select = { builtin = { win_options = { winblend = 0 } } }
+   }
 }

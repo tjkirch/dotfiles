8b32451d3b15c5609db0877e0561dc30a25a0080
Tom Kirchner <git@halffull.org>
2024-01-02 15:47:48 -0800

    switch from exa to eza

---
+++ fish/.config/fish/functions/et.fish
@@ -0,0 +1,3 @@
+function et --wraps='eza -l --time-style=iso --git-ignore --tree -I target --color-scale' --description 'alias et=eza -l --time-style=iso --git-ignore --tree -I target --color-scale'
+    eza -l --time-style=iso --git-ignore --tree -I target --color-scale $argv
+end

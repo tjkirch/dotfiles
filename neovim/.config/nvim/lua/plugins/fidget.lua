a8b937fb3a13c37e75ae4eb6856ad90116c32d46
Tom Kirchner <git@halffull.org>
2024-01-02 17:28:23 -0800

    neovim: disable fidget.nvim warning

--- neovim/.config/nvim/lua/plugins/fidget.lua
+++ neovim/.config/nvim/lua/plugins/fidget.lua
@@ -1,7 +1,9 @@
 return {
-   'j-hui/fidget.nvim',
-   lazy=true,
+   "j-hui/fidget.nvim",
+   lazy = true,
    -- I don't need any options, but for some reason fidget doesn't work without
    -- it.
    opts = {},
+   -- Disable the stupid startup warning about a pending rewrite
+   tag = "legacy",
 }

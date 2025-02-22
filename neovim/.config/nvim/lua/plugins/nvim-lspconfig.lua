11ad1ec249e5920149de810324e9f4922a19eccc
Tom Kirchner <git@halffull.org>
2025-02-21 16:01:16 -0800

    autoformat

--- neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,39 +2,39 @@
 -- are then automatically loaded to bring up a full LSP environment.
 -- (If changing this filetypeslist, update the treesitter list, too.)
 return {
-   'neovim/nvim-lspconfig',
-   lazy=true,
-   ft = {"bash", "fish", "rust", "sh", "toml", "lua"},
+   "neovim/nvim-lspconfig",
+   lazy = true,
+   ft = { "bash", "fish", "rust", "sh", "toml", "lua" },
    dependencies = {
-      'jose-elias-alvarez/null-ls.nvim',
-      'gbrlsnchs/telescope-lsp-handlers.nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'nvim-treesitter/nvim-treesitter',
-      'j-hui/fidget.nvim'
+      "jose-elias-alvarez/null-ls.nvim",
+      "gbrlsnchs/telescope-lsp-handlers.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim",
    },
    config = function()
       -- Load larger, more general LSP config file
-      vim.cmd [[runtime 90_lsp.vim]]
+      vim.cmd([[runtime 90_lsp.vim]])
       -- Rust is set up by rust-tools.lua
       -- TOML linting; null-ls only does formatting
-      require'lspconfig'.taplo.setup{}
+      require("lspconfig").taplo.setup({})
       -- lua-language-server
-      require'lspconfig'.lua_ls.setup{
+      require("lspconfig").lua_ls.setup({
          settings = {
             -- Note: these settings can be configured per Lua project with a .lurarc.json at its root
             Lua = {
                diagnostics = {
-                  globals = {'vim'}
+                  globals = { "vim" },
                },
                runtime = {
-                  version = 'Lua 5.4',
+                  version = "Lua 5.4",
                },
                telemetry = {
-                  enable = false
-               }
-            }
-         }
-      }
-   end
+                  enable = false,
+               },
+            },
+         },
+      })
+   end,
 }
--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,37 +1,39 @@
 return {
-   'nvim-treesitter/nvim-treesitter',
-   build = ':TSUpdate',
+   "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
    dependencies = {
       "HiPhish/rainbow-delimiters.nvim",
       "nvim-treesitter/nvim-treesitter-context",
    },
-   lazy=true,
+   lazy = true,
    config = function()
       --FIXME: how do I keep the list of filetypes in only one place, like nvim-lspconfig?
-      vim.cmd [[
+      vim.cmd([[
          " Treesitter-based folding, where available
          augroup treesitter
             autocmd!
             autocmd FileType bash,fish,rust,toml,lua setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()
          augroup END
-      ]]
-      require('nvim-treesitter.configs').setup {
+      ]])
+      require("nvim-treesitter.configs").setup({
          ensure_installed = { "bash", "fish", "rust", "toml", "lua" },
          auto_install = true,
          -- replace syntax highlighting / indentation with language-aware versions
          highlight = {
             enable = true,
-            additional_vim_regex_highlighting=false,
+            additional_vim_regex_highlighting = false,
          },
          indent = { enable = true },
          -- select text quickly based on language nodes/scopes
          incremental_selection = {
             enable = true,
             keymaps = {
-               init_selection = '<CR>', scope_incremental = '<CR>',
-               node_incremental = '<TAB>', node_decremental = '<S-TAB>',
+               init_selection = "<CR>",
+               scope_incremental = "<CR>",
+               node_incremental = "<TAB>",
+               node_decremental = "<S-TAB>",
             },
          },
-      }
-   end
+      })
+   end,
 }

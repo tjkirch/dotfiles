f86426da26caf0935dc1a48b9ba784b784a9edaa
Tom Kirchner <git@halffull.org>
2025-02-21 17:00:16 -0800

    fish: use new fzf --fish

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,16 +1,7 @@
 # Defined in /tmp/fish.J3VStT/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
-   # Load fzf key bindings from local/Mac/Linux paths
-   for path in "$HOME/local/src/fzf/shell/key-bindings.fish" \
-               "/usr/local/opt/fzf/shell/key-bindings.fish" \
-               "/usr/share/doc/fzf/key-bindings.fish" \
-               "/usr/share/fzf/key-bindings.fish"
-      if test -r $path
-         source $path
-         fzf_key_bindings
-         break
-      end
-   end
+    # Load fzf key bindings: ctrl-r history, ctrl-t file finder, alt-c cd
+    fzf --fish | source
 
    # ctrl+backspace to delete previous word; ctrl+w is dirty
    bind -M insert \cH backward-kill-word

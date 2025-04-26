aec3e8b7a0388c25c18b46391f4fbf3e43718710
Tom Kirchner <git@halffull.org>
2025-04-26 14:53:31 -0700

    nvim: ignore rainbow-delimiters submodules
    They keeps causing update breakage.  See:
    https://github.com/HiPhish/rainbow-delimiters.nvim/issues/173

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,10 @@
 return {
    "nvim-treesitter/nvim-treesitter",
    dependencies = {
-      "HiPhish/rainbow-delimiters.nvim",
+      {
+         "HiPhish/rainbow-delimiters.nvim",
+         submodules = false,
+      },
       {
          "nvim-treesitter/nvim-treesitter-context",
          opts = {

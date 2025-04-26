9601d9a291a28e739c50eb168f25a28b274c7cbe
Tom Kirchner <git@halffull.org>
2025-04-26 14:52:34 -0700

    Update from kickstart.nvim through d350db2449da

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,8 +5,8 @@ return {
       -- Useful status updates for LSP.
       { "j-hui/fidget.nvim", opts = {} },
 
-      -- Allows extra capabilities provided by nvim-cmp
-      "hrsh7th/cmp-nvim-lsp",
+      -- Allows extra capabilities provided by blink.cmp
+      'saghen/blink.cmp',
    },
    config = function()
       -- Brief aside: **What is LSP?**
@@ -51,10 +51,13 @@ return {
                vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
             end
 
-            -- Jump to the definition of the word under your cursor.
-            --  This is where a variable was first declared, or where a function is defined, etc.
-            --  To jump back, press <C-t>.
-            map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+            -- Rename the variable under your cursor.
+            --  Most Language Servers support renaming across files, etc.
+            map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+            -- Execute a code action, usually your cursor needs to be on top of an error
+            -- or a suggestion from your LSP for this to activate.
+            map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
             -- Find references for the word under your cursor.
             map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
@@ -63,26 +66,27 @@ return {
             --  Useful when your language has ways of declaring types without an actual implementation.
             map("gri", require("telescope.builtin").lsp_implementations, "Goto Implementation")
 
-            -- Jump to the type of the word under your cursor.
-            --  Useful when you're not sure what type a variable is and you want to see
-            --  the definition of its *type*, not where it was *defined*.
-            map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-            -- Fuzzy find all the symbols in your current document.
-            --  Symbols are things like variables, functions, types, etc.
-            map("gO", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
-
-            -- Fuzzy find all the symbols in your current workspace.
-            --  Similar to document symbols, except searches over your entire project.
-            map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-            -- Execute a code action, usually your cursor needs to be on top of an error
-            -- or a suggestion from your LSP for this to activate.
-            map("gra", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+            -- Jump to the definition of the word under your cursor.
+            --  This is where a variable was first declared, or where a function is defined, etc.
+            --  To jump back, press <C-t>.
+            map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
             -- WARN: This is not Goto Definition, this is Goto Declaration.
             --  For example, in C this would take you to the header.
-            map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+            map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+            -- Fuzzy find all the symbols in your current document.
+            --  Symbols are things like variables, functions, types, etc.
+            map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+
+            -- Fuzzy find all the symbols in your current workspace.
+            --  Similar to document symbols, except searches over your entire project.
+            map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace Symbols")
+
+            -- Jump to the type of the word under your cursor.
+            --  Useful when you're not sure what type a variable is and you want to see
+            --  the definition of its *type*, not where it was *defined*.
+            map("gD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
             -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
             ---@param client vim.lsp.Client
@@ -185,10 +189,9 @@ return {
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
--- neovim/.config/nvim/lua/plugins/which-key.lua
+++ neovim/.config/nvim/lua/plugins/which-key.lua
@@ -45,11 +45,7 @@ return {
 
       -- Document existing key chains
       spec = {
-         { "<leader>c", group = "[C]ode",     mode = { "n", "x" } },
-         { "<leader>d", group = "[D]ocument" },
-         { "<leader>r", group = "[R]ename" },
          { "<leader>s", group = "[S]earch" },
-         { "<leader>w", group = "[W]orkspace" },
          { "<leader>t", group = "[T]oggle" },
          { "<leader>g", group = "[G]it" },
       },
